6d117868ee9dace44568302ab271b04ddf8209d0
Dylan Casanova <dylan.casanova@amtil.co>
2026-06-08 11:54:36 -0500

    fix failing test.

--- tests/Feature/GarageControllerTest.php
+++ tests/Feature/GarageControllerTest.php
@@ -21,7 +21,7 @@ class GarageControllerTest extends TestCase
         $mock = Mockery::mock(DeviceInterface::class);
         $mock->shouldReceive('triggerServo')
             ->once()
-            ->with(Mockery::on(fn ($d) => $d->id === $device->id))
+            ->with(Mockery::on(fn ($d) => $d->id === $device->id), 130)
             ->andReturn(true);
         $this->app->instance(DeviceInterface::class, $mock);
 
